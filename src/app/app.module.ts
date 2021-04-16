025271127bc5f55d03f2ba88d1453b6667b4e720
Pedro Jorquera <pjorquera@okode.com>
2021-04-16 20:22:01 +0200

    Cosmetics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import '@capacitor/core';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
