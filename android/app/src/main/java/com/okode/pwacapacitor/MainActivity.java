0708b95ed903244279437de7b48ca9ac31d4dbe9
Pedro Jorquera <pjorquera@okode.com>
2021-04-19 13:37:54 +0200

    Dynamically register local plugins instead using capacitor.plugins.json

--- android/app/src/main/java/com/okode/pwacapacitor/MainActivity.java
+++ android/app/src/main/java/com/okode/pwacapacitor/MainActivity.java
@@ -1,5 +1,20 @@
 package com.okode.pwacapacitor;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import java.util.Arrays;
+
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    registerPlugins(Arrays.asList(
+      AlertPlugin.class,
+      TrackingPlugin.class
+    ));
+  }
+
+}
